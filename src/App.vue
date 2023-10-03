a114b13f0d91bd536b75759b0d54c7351988e2ca
tejas-encora <tejas.monteverdi@encora.com>
2023-10-03 11:44:53 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { VButton } from 'iodine-vuetify-lib'
+import { NonVuetify } from 'iodine-vuetify-lib'
+import { VTextInput } from 'iodine-vuetify-lib'
+import { VDatePicker } from 'vuetify/labs/VDatePicker'
+import { VDatePick } from 'iodine-vuetify-lib'
+// import 'iodine-vuetify-lib/style.css'
+import 'vuetify/dist/vuetify.min.css'
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <VTextInput />
+      <VButton />
+      <NonVuetify/>
+      <VDatePicker />
+      <VDatePick />
+    </v-main>
+  </v-app>
+</template>
+
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,26 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  {
+    theme: {
+      themes: {
+        light: {
+          colors: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107',
+            iodinePurple: '#59336D'
+          }
+        },
+      },
+    },
+}
+)
