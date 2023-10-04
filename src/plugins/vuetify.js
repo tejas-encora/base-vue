ec36aa9511a14dfd133bc64b7f1f69811cfc99d1
tejas-encora <tejas.monteverdi@encora.com>
2023-10-04 08:09:25 -0400

    add vdatepicker

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,16 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { VDatePicker } from 'vuetify/labs/VDatePicker'
+
 // Vuetify
 import { createVuetify } from 'vuetify'
 
 export default createVuetify(
   {
+    components: {
+      VDatePicker
+    },
     theme: {
       themes: {
         light: {
